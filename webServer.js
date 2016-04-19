1135a939bcf98bd9de287d37312909fad9d7bcc0
Max Krieger <themaxaxis@gmail.com>
2016-04-18 21:07:12 -0400

    basic webserver

--- webServer.js
+++ webServer.js
@@ -1,1 +1,25 @@
+"use strict";
+const express = require("express"),
+	app = express();
 
+const fs = require("fs"),
+	path = require("path");
+
+const guages = fs.readdirSync("./guages").filter((file) => {
+	return fs.statSync(path.join("./guages", file)).isDirectory();
+});
+
+console.log(guages);
+
+app.set("view engine", "pug");
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+	res.render("index", {guages});
+});
+
+app.get("/guages/:guage", (req, res) => {
+	res.render("guage", {guage: req.params.guage});
+});
+
+app.listen(3000);
