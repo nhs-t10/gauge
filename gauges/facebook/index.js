767c1167d17a806f6438e0a5fb22cb9c9e99a70b
Max Krieger <themaxaxis@gmail.com>
2016-04-20 17:15:23 -0400

    facebook

---
+++ gauges/facebook/index.js
@@ -0,0 +1,18 @@
+"use strict";
+let manifest = require("./manifest");
+const cheerio = require("cheerio");
+const request = require("request");
+
+exports.get = (callback) => {
+	request({ url: `https://www.facebook.com/${manifest.params.facebookPageId}`, headers: {
+		"User-Agent": "Mozilla/5.0"
+	}}, (err, res, body) => {
+		let $ = cheerio.load(body);
+		const likes = $("#PagesLikesCountDOMID").text().split(" ")[0];
+		callback(180/1000 * likes);
+	});
+};
+
+exports.updateParams = () => {
+	manifest = require("./manifest");
+};
